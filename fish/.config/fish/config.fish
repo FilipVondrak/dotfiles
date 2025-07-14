eb2498c25b9c4ea00534384a0febf61d2f6a2644
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-07-14 15:45:55 +0200

    feat: added custom starship prompt

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,18 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# starship settings
+function starship_transient_prompt_func
+    starship module character
+end
+function starship_transient_rprompt_func
+    starship module time
+end
+starship init fish | source
+enable_transience
+
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
