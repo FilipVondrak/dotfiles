469b2e5b0213d54efd57e0766a1300b360c4fa91
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-07-16 12:33:18 +0200

    added tide config for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,18 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+source ./tide_config.fish
 
 # starship settings
-function starship_transient_prompt_func
-    starship module character
-end
-function starship_transient_rprompt_func
-    starship module time
-end
-starship init fish | source
-enable_transience
-
+#function starship_transient_prompt_func
+#    starship module character
+#end
+#function starship_transient_rprompt_func
+#    starship module time
+#end
+#starship init fish | source
+#enable_transience
 
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
+#functon fish_greeting
+    # smth smth
 #end
---
+++ fish/.config/fish/tide_config.fish
@@ -0,0 +1,11 @@
+set -g tide_prompt_add_new_line_before false
+set -g tide_character_icon '$'
+
+# context - user@machine
+set -g tide_context_always_display true
+set -g tide_context_bg_color '#0b0b38'
+set -g tide_context_color white
+set -g tide_context_color_root red
+set -g tide_context_color_ssh red
+
+
