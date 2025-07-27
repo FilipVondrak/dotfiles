27038ba2843f538791d45d815ed167fcf67c4bdd
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-07-27 20:51:42 +0200

    feat: added zoxide to fish

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Dotfiles
+
 ## Requirements
+
 - git
 - nodejs
 - GNU Stow
 - nvim
-    - luarocks
-    - neovim
-    - nerd fonts
-    - lazygit
-    - tectonic
+  - luarocks
+  - neovim
+  - nerd fonts
+  - lazygit
+  - tectonic
+  - npm
 
 ## Usage
+
 1. clone this repo: `git clone https://github.com/FilipVondrak/dotfiles.git ~/.dotfiles && cd ~/.dotfiles`
 2. install
     - to install everything use: `stow */`
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,16 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-# source ./tide_config.fish
+
+# setup starship prompt
 source ~/.config/fish/starship_config.fish
+
+# setup zoxide
+zoxide init fish --cmd cd | source
+
+# add nix package manager to PATH
+fish_add_path ~/.nix-profile/bin
+fish_add_path ~/.local/share/JetBrains/Toolbox/scripts
 
 # overwrite greeting
 # potentially disabling fastfetch
-function fish_greeting;end
+function fish_greeting
+end
