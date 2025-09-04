633933cdd7373882a55f24cf791a1cbbf264ca04
Filip Vondrak <filip.vondrak4@seznam.cz>
2025-09-04 11:49:36 +0200

    added dotnet tools to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,12 +3,16 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # setup starship prompt
 source ~/.config/fish/starship_config.fish
 
+# setup nvm node version manager
+#bash /usr/share/nvm/init-nvm.sh
+
 # setup zoxide
 zoxide init fish --cmd cd | source
 
 # add nix package manager to PATH
 fish_add_path ~/.nix-profile/bin
 fish_add_path ~/.local/share/JetBrains/Toolbox/scripts
+fish_add_path ~/.dotnet/tools
 
 # overwrite greeting
 # potentially disabling fastfetch
